11db05c138a4319040b5908ca02278a48b5143e8
robertosuriel <robertosuriel.mr@gmail.com>
2025-04-03 13:30:11 -0300

    pip

--- atualizaSheets.py
+++ atualizaSheets.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
+os.system("pip install pandas")
 import pandas as pd
 import time
 import requests
