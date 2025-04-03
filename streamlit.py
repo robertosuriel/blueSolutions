c0cb865c88bf1ef8e3f428573aae2dcb20107fe6
robertosuriel <robertosuriel.mr@gmail.com>
2025-04-03 13:29:03 -0300

    pip

--- streamlit.py
+++ streamlit.py
@@ -1,16 +1,6 @@
 import streamlit as st
 import os
-import os
 
-os.system("pip install selenium")
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-import pandas as pd
-import time
-import requests
 
 
 st.title("Executar Script de Web Scraping")
